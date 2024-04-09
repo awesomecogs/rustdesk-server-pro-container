19f63f2ae592fe36dbe554dbbb6457709ff83433
awesomecogs <6v4dphot@emormony.com>
2024-04-08 22:25:05 -0500

    Update caps, chg zip to exe, chg null to Out-Null

--- WindowsAgentAIOInstall.ps1
+++ WindowsAgentAIOInstall.ps1
@@ -1,7 +1,7 @@
-$ErrorActionPreference= 'silentlycontinue'
+$ErrorActionPreference = 'SilentlyContinue'
 #Run as administrator and stays in the current directory
-if (-Not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+If (-Not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    If ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
         Start-Process PowerShell -Verb RunAs -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$PSCommandPath';`"";
         Exit;
     }
@@ -16,69 +16,67 @@ $rustdesk_version = $realTagUrl.split('/')[-1].Trim('v')
 Write-Output("Installing Rustdesk version $rustdesk_version")
 
 function OutputIDandPW([String]$rustdesk_id, [String]$rustdesk_pw) {
-  Write-Output("######################################################")
-  Write-Output("#                                                    #")
-  Write-Output("# CONNECTION PARAMETERS:                             #")
-  Write-Output("#                                                    #")
-  Write-Output("######################################################")
-  Write-Output("")
-  Write-Output("  RustDesk-ID:       $rustdesk_id")
-  Write-Output("  RustDesk-Password: $rustdesk_pw")
-  Write-Output("")
+    Write-Output("######################################################")
+    Write-Output("#                                                    #")
+    Write-Output("# CONNECTION PARAMETERS:                             #")
+    Write-Output("#                                                    #")
+    Write-Output("######################################################")
+    Write-Output("")
+    Write-Output("  RustDesk-ID:       $rustdesk_id")
+    Write-Output("  RustDesk-Password: $rustdesk_pw")
+    Write-Output("")
 }
 
-If (!(Test-Path $env:Temp)) {
-  New-Item -ItemType Directory -Force -Path $env:Temp > null
+If (!(Test-Path $ENV:TEMP)) {
+    New-Item -ItemType Directory -Force -Path $ENV:TEMP | Out-Null
 }
 
-If (!(Test-Path "$env:ProgramFiles\Rustdesk\RustDesk.exe")) {
+If (!(Test-Path "$ENV:ProgramFiles\Rustdesk\RustDesk.exe")) {
 
-  cd $env:Temp
+    Set-Location $ENV:TEMP
 
-  If ([Environment]::Is64BitOperatingSystem) {
-    $os_arch = "x64"
-  } Else {
-    $os_arch = "x32"
-  }
+    If ([Environment]::Is64BitOperatingSystem) {
+        $os_arch = "x64"
+    } Else {
+        $os_arch = "x32"
+    }
 
-  Invoke-WebRequest https://github.com/rustdesk/rustdesk/releases/download/$rustdesk_version/rustdesk-$rustdesk_version-windows_$os_arch.zip -Outfile rustdesk.zip
+    Invoke-WebRequest https://github.com/rustdesk/rustdesk/releases/download/$rustdesk_version/rustdesk-$rustdesk_version-windows_$os_arch.exe -Outfile rustdesk.exe
 
-  Expand-Archive rustdesk.zip
-  cd rustdesk
-  Start-Process "rustdesk-$rustdesk_version-putes.exe" -argumentlist "--silent-install" -wait
+    Start-Process -FilePath "$($ENV:TEMP)\rustdesk.exe" -ArgumentList "--silent-install" -Wait
 
-  # Set URL Handler
-  New-Item -Path "HKLM:\SOFTWARE\Classes\RustDesk" > null
-  Set-ItemProperty -Path "HKLM:\SOFTWARE\Classes\RustDesk" -Name "(Default)" -Value "URL:RustDesk Protocol" > null
-  New-ItemProperty -Path "HKLM:\SOFTWARE\Classes\RustDesk" -Name "URL Protocol" -Type STRING > null
+    # Set URL Handler
+    New-Item -Path "HKLM:\SOFTWARE\Classes\RustDesk" | Out-Null
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Classes\RustDesk" -Name "(Default)" -Value "URL:RustDesk Protocol" | Out-Null
+    New-ItemProperty -Path "HKLM:\SOFTWARE\Classes\RustDesk" -Name "URL Protocol" -Type STRING | Out-Null
 
-  New-Item -Path "HKLM:\SOFTWARE\Classes\RustDesk\DefaultIcon" > null
-  Set-ItemProperty -Path "HKLM:\SOFTWARE\Classes\RustDesk\DefaultIcon" -Name "(Default)" -Value "RustDesk.exe,0" > null
+    New-Item -Path "HKLM:\SOFTWARE\Classes\RustDesk\DefaultIcon" | Out-Null
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Classes\RustDesk\DefaultIcon" -Name "(Default)" -Value "RustDesk.exe,0" | Out-Null
 
-  New-Item -Path "HKLM:\SOFTWARE\Classes\RustDesk\shell" > null
-  New-Item -Path "HKLM:\SOFTWARE\Classes\RustDesk\shell\open" > null
-  New-Item -Path "HKLM:\SOFTWARE\Classes\RustDesk\shell\open\command" > null
-  $rustdesklauncher = '"' + $env:ProgramFiles + '\RustDesk\RustDeskURLLauncher.exe" %1"'
-  Set-ItemProperty -Path "HKLM:\SOFTWARE\Classes\RustDesk\shell\open\command" -Name "(Default)" -Value $rustdesklauncher > null
+    New-Item -Path "HKLM:\SOFTWARE\Classes\RustDesk\shell" | Out-Null
+    New-Item -Path "HKLM:\SOFTWARE\Classes\RustDesk\shell\open" | Out-Null
+    New-Item -Path "HKLM:\SOFTWARE\Classes\RustDesk\shell\open\command" | Out-Null
+    $rustdesklauncher = '"' + $ENV:ProgramFiles + '\RustDesk\RustDeskURLLauncher.exe" %1"'
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Classes\RustDesk\shell\open\command" -Name "(Default)" -Value $rustdesklauncher | Out-Null
 
-  Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force > null
-  Install-Module ps2exe -Force > null
+    Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force > null
+    Install-Module ps2exe -Force > null
 
 $urlhandler_ps1 = @"
-  `$url_handler = `$args[0]
-  `$rustdesk_id = `$url_handler -creplace '(?s)^.*\:',''
-  Start-Process -FilePath '$env:ProgramFiles\RustDesk\rustdesk.exe' -ArgumentList "--connect `$rustdesk_id"
+    `$url_handler = `$args[0]
+    `$rustdesk_id = `$url_handler -creplace '(?s)^.*\:',''
+    Start-Process -FilePath '$ENV:ProgramFiles\RustDesk\rustdesk.exe' -ArgumentList "--connect `$rustdesk_id"
 "@
 
-  New-Item "$env:ProgramFiles\RustDesk\urlhandler.ps1" > null
-  Set-Content "$env:ProgramFiles\RustDesk\urlhandler.ps1" $urlhandler_ps1 > null
-  Invoke-Ps2Exe "$env:ProgramFiles\RustDesk\urlhandler.ps1" "$env:ProgramFiles\RustDesk\RustDeskURLLauncher.exe" > null
+    New-Item "$ENV:ProgramFiles\RustDesk\urlhandler.ps1" | Out-Null
+    Set-Content "$ENV:ProgramFiles\RustDesk\urlhandler.ps1" $urlhandler_ps1 | Out-Null
+    Invoke-Ps2Exe "$ENV:ProgramFiles\RustDesk\urlhandler.ps1" "$ENV:ProgramFiles\RustDesk\RustDeskURLLauncher.exe" | Out-Null
 
-  # Cleanup Tempfiles
-  Remove-Item "$env:ProgramFiles\RustDesk\urlhandler.ps1" > null
-  cd $env:Temp
-  Remove-Item $env:Temp\rustdesk -Recurse > null
-  Remove-Item $env:Temp\rustdesk.zip > null
+    # Cleanup Tempfiles
+    Remove-Item "$ENV:ProgramFiles\RustDesk\urlhandler.ps1" | Out-Null
+    Set-Location -Path $ENV:TEMP
+    Remove-Item $ENV:TEMP\rustdesk -Recurse | Out-Null
+    Remove-Item $ENV:TEMP\rustdesk.zip | Out-Null
 }
 
 # Write config
@@ -89,42 +87,42 @@ serial = 0
 
 [options]
 custom-rendezvous-server = 'wanipreg'
-key =  'keyreg'
+key = 'keyreg'
 relay-server = 'wanipreg'
 api-server = 'https://wanipreg'
 enable-audio = 'N'
 direct-server = 'Y'
 "@
 
-If (!(Test-Path $env:AppData\RustDesk\config\RustDesk2.toml)) {
-  New-Item $env:AppData\RustDesk\config\RustDesk2.toml > null
+If (!(Test-Path $ENV:AppData\RustDesk\config\RustDesk2.toml)) {
+    New-Item $ENV:AppData\RustDesk\config\RustDesk2.toml | Out-Null
 }
-Set-Content $env:AppData\RustDesk\config\RustDesk2.toml $RustDesk2_toml > null
+Set-Content $ENV:AppData\RustDesk\config\RustDesk2.toml $RustDesk2_toml | Out-Null
 
-If (!(Test-Path $env:WinDir\ServiceProfiles\LocalService\AppData\Roaming\RustDesk\config\RustDesk2.toml)) {
-  New-Item $env:WinDir\ServiceProfiles\LocalService\AppData\Roaming\RustDesk\config\RustDesk2.toml > null
+If (!(Test-Path $ENV:WinDir\ServiceProfiles\LocalService\AppData\Roaming\RustDesk\config\RustDesk2.toml)) {
+    New-Item $ENV:WinDir\ServiceProfiles\LocalService\AppData\Roaming\RustDesk\config\RustDesk2.toml | Out-Null
 }
-Set-Content $env:WinDir\ServiceProfiles\LocalService\AppData\Roaming\RustDesk\config\RustDesk2.toml $RustDesk2_toml > null
+Set-Content $ENV:WinDir\ServiceProfiles\LocalService\AppData\Roaming\RustDesk\config\RustDesk2.toml $RustDesk2_toml | Out-Null
 
 $random_pass = (-join ((65..90) + (97..122) | Get-Random -Count 8 | % {[char]$_}))
-Start-Process "$env:ProgramFiles\RustDesk\RustDesk.exe"  -argumentlist "--password $random_pass" -wait
+Start-Process "$ENV:ProgramFiles\RustDesk\RustDesk.exe"  -argumentlist "--password $random_pass" -wait
 
 # Get RustDesk ID
-If (!("$env:WinDir\ServiceProfiles\LocalService\AppData\Roaming\RustDesk\config\RustDesk.toml")) {
-  $rustdesk_id = (Get-Content $env:AppData\RustDesk\config\RustDesk.toml | Where-Object { $_.Contains("id") })
-  $rustdesk_id = $rustdesk_id.Split("'")[1]
-  $rustdesk_pw = (Get-Content $env:AppData\RustDesk\config\RustDesk.toml | Where-Object { $_.Contains("password") })
-  $rustdesk_pw = $rustdesk_pw.Split("'")[1]
-  Write-Output("Config file found in user folder")
-  OutputIDandPW $rustdesk_id $rustdesk_pw
+If (!("$ENV:WinDir\ServiceProfiles\LocalService\AppData\Roaming\RustDesk\config\RustDesk.toml")) {
+    $rustdesk_id = (Get-Content $ENV:AppData\RustDesk\config\RustDesk.toml | Where-Object { $_.Contains("id") })
+    $rustdesk_id = $rustdesk_id.Split("'")[1]
+    $rustdesk_pw = (Get-Content $ENV:AppData\RustDesk\config\RustDesk.toml | Where-Object { $_.Contains("password") })
+    $rustdesk_pw = $rustdesk_pw.Split("'")[1]
+    Write-Output("Config file found in user folder")
+    OutputIDandPW $rustdesk_id $rustdesk_pw
 } Else {
-  $rustdesk_id = (Get-Content $env:WinDir\ServiceProfiles\LocalService\AppData\Roaming\RustDesk\config\RustDesk.toml | Where-Object { $_.Contains("id") })
-  $rustdesk_id = $rustdesk_id.Split("'")[1]
-  $rustdesk_pw = (Get-Content $env:WinDir\ServiceProfiles\LocalService\AppData\Roaming\RustDesk\config\RustDesk.toml | Where-Object { $_.Contains("password") })
-  $rustdesk_pw = $rustdesk_pw.Split("'")[1]
-  Write-Output "Config file found in windows service folder"
-  OutputIDandPW $rustdesk_id $rustdesk_pw
+    $rustdesk_id = (Get-Content $ENV:WinDir\ServiceProfiles\LocalService\AppData\Roaming\RustDesk\config\RustDesk.toml | Where-Object { $_.Contains("id") })
+    $rustdesk_id = $rustdesk_id.Split("'")[1]
+    $rustdesk_pw = (Get-Content $ENV:WinDir\ServiceProfiles\LocalService\AppData\Roaming\RustDesk\config\RustDesk.toml | Where-Object { $_.Contains("password") })
+    $rustdesk_pw = $rustdesk_pw.Split("'")[1]
+    Write-Output "Config file found in windows service folder"
+    OutputIDandPW $rustdesk_id $rustdesk_pw
 }
 
-Stop-Process -Name RustDesk -Force > null
-Start-Service -Name RustDesk > null
+Stop-Process -Name RustDesk -Force | Out-Null
+Start-Service -Name RustDesk | Out-Null
